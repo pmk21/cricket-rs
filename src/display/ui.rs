37fdb2496ee545cff372d2c29f5be553d27394c4
pmk21 <prithvikrishna49@gmail.com>
2021-05-09 18:24:15 +0530

    Added base implementation of UI

--- src/display/ui.rs
+++ src/display/ui.rs
@@ -1,12 +1,16 @@
-use crate::app::App;
+use std::collections::HashMap;
+
+use crate::{app::App, cricbuzz_api::CricbuzzMiniscoreMatchScoreDetailsInningsScore};
 use tui::{
     backend::Backend,
-    layout::{Constraint, Layout, Rect},
-    Frame,
+    layout::{Constraint, Layout, Rect, Direction},
     style::{Color, Modifier, Style},
     text::{Span, Spans},
-    widgets::{Block, Borders, Paragraph, Tabs, Wrap},
+    widgets::{Block, Borders, Paragraph, Row, Table, Tabs},
+    Frame,
 };
+
+use crate::cricbuzz_api::CricbuzzJson;
 
 pub fn draw_ui<B>(f: &mut Frame<B>, app: &App) -> ()
 where
@@ -15,66 +19,291 @@ where
     let chunks = Layout::default()
         .constraints([Constraint::Length(3), Constraint::Min(0)].as_ref())
         .split(f.size());
-    
+
     let tab_titles = app
-    .matches_info
-    .iter()
-    .map(|m| Spans::from(Span::styled(m.match_short_name.as_str(), Style::default().fg(Color::Green))))
-    .collect();
+        .matches_info
+        .iter()
+        .map(|m| {
+            Spans::from(Span::styled(
+                m.match_short_name.as_str(),
+                Style::default().fg(Color::Green),
+            ))
+        })
+        .collect();
 
     let tabs = Tabs::new(tab_titles)
-        .block(Block::default().borders(Borders::ALL).title("Block Title"))
+        .block(Block::default().borders(Borders::ALL).title("Matches"))
         .highlight_style(Style::default().fg(Color::Yellow))
         .select(app.focused_tab as usize);
     f.render_widget(tabs, chunks[0]);
-    draw_tab(f, chunks[1]);
+    draw_tab(f, chunks[1], app);
 }
 
-fn draw_tab<B>(f: &mut Frame<B>, area: Rect)
+fn draw_tab<B>(f: &mut Frame<B>, area: Rect, app: &App)
 where
     B: Backend,
 {
-    let _chunks = Layout::default()
-        .constraints(
-            [
-                Constraint::Length(9),
-                Constraint::Min(8),
-                Constraint::Length(7),
-            ]
-            .as_ref(),
-        )
+    let chunks = Layout::default()
+        .constraints([Constraint::Length(5), Constraint::Percentage(88)].as_ref())
         .split(area);
 
-    let text = vec![
-            Spans::from("This is a paragraph with several lines. You can change style your text the way you want"),
-            Spans::from(""),
-            Spans::from(vec![
-                Span::from("For example: "),
-                Span::styled("under", Style::default().fg(Color::Red)),
-                Span::raw(" "),
-                Span::styled("the", Style::default().fg(Color::Green)),
-                Span::raw(" "),
-                Span::styled("rainbow", Style::default().fg(Color::Blue)),
-                Span::raw("."),
-            ]),
-            Spans::from(vec![
-                Span::raw("Oh and if you didn't "),
-                Span::styled("notice", Style::default().add_modifier(Modifier::ITALIC)),
-                Span::raw(" you can "),
-                Span::styled("automatically", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" "),
-                Span::styled("wrap", Style::default().add_modifier(Modifier::REVERSED)),
-                Span::raw(" your "),
-                Span::styled("text", Style::default().add_modifier(Modifier::UNDERLINED)),
-                Span::raw(".")
-            ]),
-        ];
-    let block = Block::default().borders(Borders::ALL).title(Span::styled(
-        "Footer",
-        Style::default()
-            .fg(Color::Magenta)
-            .add_modifier(Modifier::BOLD),
-    ));
-    let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true });
-    f.render_widget(paragraph, area);
+    let curr_match = &app.matches_info[app.focused_tab as usize].cricbuzz_info;
+
+    let scores = get_match_summary_info(curr_match);
+
+    let summ_block = Block::default().borders(Borders::ALL).title("Summary");
+    let paragraph = Paragraph::new(scores).block(summ_block);
+    f.render_widget(paragraph, chunks[0]);
+    draw_scorecard(f, chunks[1], app);
+}
+
+fn draw_scorecard<B>(f: &mut Frame<B>, area: Rect, app: &App)
+where
+    B: Backend,
+{
+    let chunks = Layout::default()
+        .constraints([Constraint::Percentage(70), Constraint::Percentage(30)].as_ref())
+        .direction(Direction::Horizontal)
+        .split(area);
+
+    let curr_match = &app.matches_info[app.focused_tab as usize].cricbuzz_info;
+
+    let table = Table::new(vec![
+        Row::new(vec!["Batsman", "R", "B", "4", "6", "SR"]),
+        Row::new(vec![
+            curr_match.miniscore.batsman_striker.bat_name.to_string() + " *",
+            curr_match.miniscore.batsman_striker.bat_runs.to_string(),
+            curr_match.miniscore.batsman_striker.bat_balls.to_string(),
+            curr_match.miniscore.batsman_striker.bat_fours.to_string(),
+            curr_match.miniscore.batsman_striker.bat_sixes.to_string(),
+            curr_match
+                .miniscore
+                .batsman_striker
+                .bat_strike_rate
+                .to_string(),
+        ]),
+        Row::new(vec![
+            curr_match
+                .miniscore
+                .batsman_non_striker
+                .bat_name
+                .to_string(),
+            curr_match
+                .miniscore
+                .batsman_non_striker
+                .bat_runs
+                .to_string(),
+            curr_match
+                .miniscore
+                .batsman_non_striker
+                .bat_balls
+                .to_string(),
+            curr_match
+                .miniscore
+                .batsman_non_striker
+                .bat_fours
+                .to_string(),
+            curr_match
+                .miniscore
+                .batsman_non_striker
+                .bat_sixes
+                .to_string(),
+            curr_match
+                .miniscore
+                .batsman_non_striker
+                .bat_strike_rate
+                .to_string(),
+        ]),
+        Row::new(vec!["Bowler", "O", "M", "R", "W", "ECO"]),
+        Row::new(vec![
+            curr_match.miniscore.bowler_striker.bowl_name.to_string() + " *",
+            curr_match.miniscore.bowler_striker.bowl_ovs.to_string(),
+            curr_match.miniscore.bowler_striker.bowl_maidens.to_string(),
+            curr_match.miniscore.bowler_striker.bowl_runs.to_string(),
+            curr_match.miniscore.bowler_striker.bowl_wkts.to_string(),
+            curr_match.miniscore.bowler_striker.bowl_econ.to_string(),
+        ]),
+        Row::new(vec![
+            curr_match
+                .miniscore
+                .bowler_non_striker
+                .bowl_name
+                .to_string(),
+            curr_match.miniscore.bowler_non_striker.bowl_ovs.to_string(),
+            curr_match
+                .miniscore
+                .bowler_non_striker
+                .bowl_maidens
+                .to_string(),
+            curr_match
+                .miniscore
+                .bowler_non_striker
+                .bowl_runs
+                .to_string(),
+            curr_match
+                .miniscore
+                .bowler_non_striker
+                .bowl_wkts
+                .to_string(),
+            curr_match
+                .miniscore
+                .bowler_non_striker
+                .bowl_econ
+                .to_string(),
+        ]),
+    ])
+    .style(Style::default().fg(Color::White))
+    .block(Block::default().borders(Borders::ALL).title("Scorecard"))
+    .widths(&[
+        Constraint::Length(25),
+        Constraint::Length(5),
+        Constraint::Length(5),
+        Constraint::Length(5),
+        Constraint::Length(5),
+        Constraint::Length(6),
+    ]);
+
+    f.render_widget(table, chunks[0]);
+}
+
+fn get_match_summary_info(match_info: &CricbuzzJson) -> Vec<Spans> {
+    let msd = &match_info.miniscore.match_score_details;
+    let mut scores = vec![];
+
+    if msd.match_format == "TEST" {
+        let total_inngs = msd.innings_score_list.len();
+        if total_inngs == 1 {
+            if msd.innings_score_list[0].is_declared == true {
+                scores.push(Spans::from(vec![
+                    Span::styled(
+                        msd.innings_score_list[0].bat_team_name.as_str(),
+                        Style::default().add_modifier(Modifier::BOLD | Modifier::ITALIC),
+                    ),
+                    Span::styled(
+                        msd.innings_score_list[0].score.to_string() + "/",
+                        Style::default().add_modifier(Modifier::BOLD | Modifier::ITALIC),
+                    ),
+                    Span::styled(
+                        msd.innings_score_list[0].wickets.to_string(),
+                        Style::default().add_modifier(Modifier::BOLD | Modifier::ITALIC),
+                    ),
+                    Span::styled(
+                        "d",
+                        Style::default().add_modifier(Modifier::BOLD | Modifier::ITALIC),
+                    ),
+                ]));
+            } else {
+                scores.push(Spans::from(vec![
+                    Span::styled(
+                        msd.innings_score_list[0].bat_team_name.as_str(),
+                        Style::default().add_modifier(Modifier::BOLD | Modifier::ITALIC),
+                    ),
+                    Span::styled(
+                        msd.innings_score_list[0].score.to_string() + "/",
+                        Style::default().add_modifier(Modifier::BOLD | Modifier::ITALIC),
+                    ),
+                    Span::styled(
+                        msd.innings_score_list[0].wickets.to_string(),
+                        Style::default().add_modifier(Modifier::BOLD | Modifier::ITALIC),
+                    ),
+                    Span::styled(
+                        msd.innings_score_list[0].score.to_string(),
+                        Style::default().add_modifier(Modifier::BOLD | Modifier::ITALIC),
+                    ),
+                ]));
+            }
+        } else if total_inngs == 2 {
+            let mut teams: HashMap<&str, Vec<&CricbuzzMiniscoreMatchScoreDetailsInningsScore>> =
+                HashMap::new();
+
+            let bat_team_name = msd.match_team_info[1].batting_team_short_name.as_str();
+            let bowl_team_name = msd.match_team_info[1].bowling_team_short_name.as_str();
+
+            for inns_score in &msd.innings_score_list {
+                if inns_score.innings_id == 1 {
+                    teams
+                        .entry(inns_score.bat_team_name.as_str())
+                        .or_insert_with(Vec::new)
+                        .push(inns_score);
+                } else if inns_score.innings_id == 2 {
+                    teams
+                        .entry(inns_score.bat_team_name.as_str())
+                        .or_insert_with(Vec::new)
+                        .push(inns_score);
+                }
+            }
+
+            scores.push(Spans::from(vec![Span::styled(
+                format!(
+                    "{} {}/{}",
+                    bat_team_name,
+                    teams[bat_team_name][0].score.to_string(),
+                    teams[bat_team_name][0].wickets.to_string(),
+                ),
+                Style::default().add_modifier(Modifier::BOLD),
+            )]));
+
+            scores.push(Spans::from(vec![Span::styled(
+                format!(
+                    "{} {}/{}",
+                    bowl_team_name,
+                    teams[bowl_team_name][0].score.to_string(),
+                    teams[bowl_team_name][0].wickets.to_string(),
+                ),
+                Style::default(),
+            )]));
+        } else if total_inngs == 3 {
+            let mut teams: HashMap<&str, Vec<&CricbuzzMiniscoreMatchScoreDetailsInningsScore>> =
+                HashMap::new();
+
+            let bat_team_name = msd.match_team_info[2].batting_team_short_name.as_str();
+            let bowl_team_name = msd.match_team_info[2].bowling_team_short_name.as_str();
+
+            for inns_score in &msd.innings_score_list {
+                if inns_score.innings_id == 1 {
+                    teams
+                        .entry(inns_score.bat_team_name.as_str())
+                        .or_insert_with(Vec::new)
+                        .push(inns_score);
+                } else if inns_score.innings_id == 2 {
+                    teams
+                        .entry(inns_score.bat_team_name.as_str())
+                        .or_insert_with(Vec::new)
+                        .push(inns_score);
+                } else if inns_score.innings_id == 3 {
+                    teams
+                        .entry(inns_score.bat_team_name.as_str())
+                        .or_insert_with(Vec::new)
+                        .push(inns_score);
+                }
+            }
+
+            scores.push(Spans::from(vec![Span::styled(
+                format!(
+                    "{} {}/{} & {}/{}",
+                    bat_team_name,
+                    teams[bat_team_name][0].score.to_string(),
+                    teams[bat_team_name][0].wickets.to_string(),
+                    teams[bat_team_name][1].score.to_string(),
+                    teams[bat_team_name][1].wickets.to_string(),
+                ),
+                Style::default().add_modifier(Modifier::BOLD),
+            )]));
+
+            scores.push(Spans::from(vec![Span::styled(
+                format!(
+                    "{} {}/{}",
+                    bowl_team_name,
+                    teams[bowl_team_name][0].score.to_string(),
+                    teams[bowl_team_name][0].wickets.to_string(),
+                ),
+                Style::default(),
+            )]));
+        } else {
+        }
+    }
+
+    scores.push(Spans::from(Span::styled(msd.custom_status.as_str(), Style::default().fg(Color::LightRed).add_modifier(Modifier::DIM))));
+
+    scores
 }
